fb5cbb8dc88c843d33fa85d9adc306d656e4816d
khang <khang@example.com>
2025-04-19 18:58:49 +0700

    fourth

--- models/User.js
+++ models/User.js
@@ -1,10 +1,9 @@
-// filepath: e:\Chuyên đề lập trình web\tuan10\tuan100\server-side\models\User.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  tokens: [{ token: { type: String, required: true } }], // Lưu danh sách token
+  refreshTokens: [{ token: { type: String } }], // Lưu danh sách Refresh Token
 });
 
 const User = mongoose.model('User', userSchema);
